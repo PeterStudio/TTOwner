2c24a13093f6d3a1e9abc12106377514bed1573c
peter <peterstudio@icloud.com>
2015-07-23 09:30:19 +0800

    update

---
+++ TTOwner/ViewController/MainVC/TTBaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTBaseViewController.h
+//  TTOwner
+//
+//  Created by Baby on 15/7/23.
+//  Copyright (c) 2015年 duwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTBaseViewController : UIViewController
+
+@end
---
+++ TTOwner/ViewController/MainVC/TTHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTHomeViewController.h
+//  TTOwner
+//
+//  Created by Baby on 15/7/23.
+//  Copyright (c) 2015年 duwen. All rights reserved.
+//
+
+#import "TTBaseViewController.h"
+
+@interface TTHomeViewController : TTBaseViewController
+
+@end
